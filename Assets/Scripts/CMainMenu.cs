dffba9b29dd0267b980a7513453c34332c34edfb
CatNBox <damian.bonggi@gmail.com>
2014-11-01 05:06:21 +0900

    20141101 MainMenu work
    1. Made templet of maintitle
    2. In processing mainmenu

--- Assets/Scripts/CMainMenu.cs
+++ Assets/Scripts/CMainMenu.cs
@@ -2,14 +2,27 @@
 using System.Collections;
 
 public class CMainMenu : MonoBehaviour {
+	Texture bg;
+	float ScrWidth;
+	float ScrHeight;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.bg = Resources.Load ("MainMenuUI/background") as Texture;
+
+		this.ScrWidth = Screen.width/1280.0f;
+		this.ScrHeight = Screen.height/720.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	
+	void OnGUI()
+	{
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), this.bg);
+	}
+
 }
--- Assets/Scripts/CMainTitle.cs
+++ Assets/Scripts/CMainTitle.cs
@@ -2,14 +2,57 @@
 using System.Collections;
 
 public class CMainTitle : MonoBehaviour {
+	Texture bg;
+	Texture TouchImg;
+	GameObject MainMenu;
+	float Alpa4Fade;
+	float AlpaDir;
+	float Time4Fade;
+	float ScrWidth;
+	float ScrHeight;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.bg = Resources.Load ("MainMenuUI/background") as Texture;
+		this.TouchImg = Resources.Load ("MainMenuUI/TouchImg") as Texture;
+
+		this.MainMenu = GameObject.Find ("MainMenu");
+		this.MainMenu.SetActive (false);
+
+		this.Alpa4Fade = 1.0f;
+		this.AlpaDir = 0.1f;
+		this.ScrWidth = Screen.width/1280.0f;
+		this.ScrHeight = Screen.height/720.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			this.MainMenu.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+
+	void OnGUI()
+	{
+		Time4Fade += Time.deltaTime;
+		if(Time4Fade >= 0.1f)
+		{
+			if (Alpa4Fade <= 0) 
+			{
+				AlpaDir = 0.05f;
+			} 
+			else if (Alpa4Fade >= 1) 
+			{
+				AlpaDir = -0.05f;
+			}
+			Alpa4Fade += AlpaDir;
+			Time4Fade = 0;
+		}
+
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), this.bg);
+		GUI.color = new Color (0, 0, 0, Alpa4Fade); 
+		GUI.DrawTexture (new Rect ((Screen.width*0.3f), (Screen.height*0.35f), this.TouchImg.width*ScrWidth, this.TouchImg.height*ScrHeight), this.TouchImg);
 	}
 }
