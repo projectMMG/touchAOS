e12eeac2b114e55e6abc35900bba8a15babad9eb
Gslazer <gslazer2@gmail.com>
2015-01-25 12:17:18 +0900

    Character Cloning
    character cloning, select UI, etc...

--- Assets/Scripts/CCameraObject.cs
+++ Assets/Scripts/CCameraObject.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 
 public class CCameraObject : MonoBehaviour {
-
+	public GameObject selectedCharacter;
+	public float speed;
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	void MoveCamera ()
+	{
+		Vector3 nextPos;
+		nextPos=new Vector3(selectedCharacter.transform.position.x-transform.position.x,selectedCharacter.transform.position.y-transform.position.y,0);
+		nextPos.Normalize();
+		transform.Translate(nextPos*speed);
 	}
 	
 	// Update is called once per frame
@@ -13,7 +22,11 @@ public class CCameraObject : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
+		if (Vector2.Distance (selectedCharacter.transform.position, transform.position) > 1.0) {
+			
+			//Debug.Log ("Camera distance " + Vector2.Distance ( selectedCharacter.transform.position, transform.position));
+			MoveCamera ();
+		}
 	}
 }
 
