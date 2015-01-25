9178d3238e44f79e46823adfe54a7e59e929d85e
roommy2 <roommy2@naver.com>
2015-01-25 14:34:58 +0900

    Revert "Revert "Revert "Revert "Character Cloning""""
    This reverts commit 551a3a7635e6c25598a825341e1041ece54cfcf3.

--- Assets/Scripts/CCharacter.cs
+++ Assets/Scripts/CCharacter.cs
@@ -4,26 +4,31 @@ using System.Collections;
 public class CCharacter : MonoBehaviour {
 	
 	public float speed;
-	Vector3 direction;
+	Vector3 destination;
 	
 	// Use this for initialization
 	void Start () {
-	
+		destination = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0))
-			ScreentoWorld();
-		MoveCharacter ();
+		if (Input.GetMouseButtonDown (0)) {
+			ScreentoWorld ();
+		}
+		if (Vector2.Distance (destination, transform.position) > 1.0) {
+			
+			//Debug.Log (Vector2.Distance ( destination, transform.position));
+			MoveCharacter ();
+		}
 	}
 	public void ScreentoWorld(){
 		Vector3 worldPos = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane) );
-		direction=worldPos;
+		destination=worldPos;
 	}
 	public void MoveCharacter(){
 		Vector3 nextPos;
-		nextPos=new Vector3(direction.x-transform.position.x,direction.y-transform.position.y,0);
+		nextPos=new Vector3(destination.x-transform.position.x,destination.y-transform.position.y,0);
 		nextPos.Normalize();
 		transform.Translate(nextPos*speed);
 	}
